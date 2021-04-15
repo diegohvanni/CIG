373c1cae02c6a1cd8754eafae4f472dc89441b76
diegohvanni <diegoh.vanni@gmail.com>
2021-04-15 09:39:21 -0700

    v 1.1
    - Added window title
    - Changed the output file encoding from UTF-8 to UTF-8+BOM
    - added option to process or not the en_source folder.

--- CIG.py
+++ CIG.py
@@ -8,32 +8,42 @@ import tkinter as tk
 #GUI STARTS
 
 window = tk.Tk()
-canvas = tk.Canvas(window, width = 400, height = 300,  relief = 'raised')
+window.title("CONTENT Inserts Generator (CIG)")
+processSource = tk.IntVar() #used for processing "en_source" file
+canvas = tk.Canvas(window, width = 450, height = 300,  relief = 'raised')
 canvas.pack()
 
-title = tk.Label(window, text='Content Inserts Generator (CIG)')
-title.config(font=('helvetica', 20))
+title = tk.Label(window, text='CONTENT Inserts Generator (CIG)')
+title.config(font=('helvetica', 22))
 canvas.create_window(200, 25, window=title)
 
-fieldLabel0 = tk.Label(window, text='Enter the \"CONTENT\" ticket number:')
-fieldLabel0.config(font=('helvetica', 10))
-canvas.create_window(100, 100, window=fieldLabel0)
+fieldLabel1 = tk.Label(window, text='Enter the \"CONTENT\" ticket number:')
+fieldLabel1.config(font=('helvetica', 12))
+canvas.create_window(100, 100, window=fieldLabel1)
 
-entry1 = tk.Entry (window)
+entry1 = tk.Entry(window)
 canvas.create_window(100, 120, window=entry1)
 
-fieldLabel2 = tk.Label(window, text='Enter path for \"translation-l10n\" folder: ')
-fieldLabel2.config(font=('helvetica', 10))
-canvas.create_window(100, 150, window=fieldLabel2)
+fieldLabel2 = tk.Label(window, text='Enter path for translations folder: ')
+fieldLabel2.config(font=('helvetica', 12))
+canvas.create_window(90, 150, window=fieldLabel2)
 
-entry2 = tk.Entry (window)
+entry2 = tk.Entry(window)
 canvas.create_window(100, 170, window=entry2)
 
+fieldLabel3 = tk.Label(window, text='Process \"en_source\" file')
+fieldLabel3.config(font=('helvetica', 12))
+canvas.create_window(350, 130, window=fieldLabel3)
+
+entry3 = tk.Checkbutton(window, variable=processSource, onvalue=1, offvalue=0)
+canvas.create_window(350, 150, window=entry3)
+
 signature = tk.Label(window, text='by Diego H. Vanni - dvanni@paypal.com')
-signature.config(font=('helvetica', 10))
-canvas.create_window(300, 290, window=signature)
+signature.config(font=('helvetica', 12))
+canvas.create_window(350, 290, window=signature)
 
 # GUI ENDS
+
 
 #defining "cig" function
 def cig():
@@ -41,11 +51,11 @@ def cig():
     ticketNumber = entry1.get() #getting Ticket Number from GUI
     URL = entry2.get() #getting Folder Path from GUI
 
-
 #MAIN PROCESSING STARTS
 
     #creating output file using "UTF-8" encoding to avoid issues with special char
-    o = open(URL+"/CONTENT - "+ticketNumber+" - DSQL.txt","w+", 1,"utf-8", "Save File Error!")
+    #o = open(URL+"/CONTENT - "+ticketNumber+" - DSQL.txt","w+", 1,"utf-8", "Save File Error!")
+    o = open(URL+"/CONTENT - "+ticketNumber+" - DSQL.txt","w+", 1,"utf-8-sig", "Save File Error!") #generating UTF-8+BOM
 
     #capturing folder structure for all available languages
     languages = {}
@@ -59,55 +69,55 @@ def cig():
 
 
 #Processing English
+    if processSource.get() == 1:
 
-    data = {} #defining Empty Dictionary Object
+        data = {} #defining Empty Dictionary Object
 
-    #parsing from file with exception
-    try:
-        with open(URL+"/en_source/hw_translation_file.json") as e:
-            data = json.load(e)
-    except ValueError:
-        o.write("Bad JSON for: en")
+        #parsing from file with exception
+        try:
+            with open(URL+"/en_source/hw_translation_file.json") as e:
+                data = json.load(e)
+        except ValueError:
+            o.write("Bad JSON for: en")
 
-    #declaring/setting single use variables
-    totalKeys = len(data['content']) #get total keys
-    walletNumber = data.get("walletNumber") #get walletNumber
-    channel = data.get("channel") #get channel
+        #declaring/setting single use variables
+        totalKeys = len(data['content']) #get total keys
+        walletNumber = data.get("walletNumber") #get walletNumber
+        channel = data.get("channel") #get channel
 
-    #iterating through all available keys on english file
-    for x in range(totalKeys):
-        if data['content'][x]['type'] == "Message_Body":
-            html = 1 #is "message_body"
-        else:
-            html = 0 #is NOT "message_body"
+        #iterating through all available keys on english file
+        for x in range(totalKeys):
+            if data['content'][x]['type'] == "Message_Body":
+                html = 1 #is "message_body"
+            else:
+                html = 0 #is NOT "message_body"
 
-        if data['content'][x]['description'] == "":
-            desc = "null" #description is null/empty
-        else:
-            desc = "'"+data['content'][x]['description']+"'" #description is not null/empty
+            if data['content'][x]['description'] == "":
+                desc = "null" #description is null/empty
+            else:
+                desc = "'"+data['content'][x]['description']+"'" #description is not null/empty
 
-        #composing and writing output to file
-        o.write("REPLACE INTO content_translations (keyName, walletNumber, type, status, description, channel, locale, content, fileContent, fileExtension, htmlEmailMessageType, serviceCallerIdentificationId, creationDate, latUpdated, version, migratedContent, migratedNonSecureContent, legacyUrls) VALUES ("
-                +"'"+data['content'][x]['keyName']+"',"
-                +str(walletNumber)+","
-                +"'"+data['content'][x]['type']+"',"
-                +"'"+"ENABLED"+"',"
-                +desc+","
-                +"'"+channel+"',"
-                +"'en',"
-                +"'"+data['content'][x]['content']+"',"
-                +"null,"
-                +"null,"
-                +str(html)+","
-                +str(0)+","
-                +"now()"+","
-                +"now(),"
-                +"1,"
-                +"null,"
-                +"null,"
-                +"null"
-                +");\n\n")
-
+            #composing and writing output to file
+            o.write("REPLACE INTO content_translations (keyName, walletNumber, type, status, description, channel, locale, content, fileContent, fileExtension, htmlEmailMessageType, serviceCallerIdentificationId, creationDate, latUpdated, version, migratedContent, migratedNonSecureContent, legacyUrls) VALUES ("
+                    +"'"+data['content'][x]['keyName']+"',"
+                    +str(walletNumber)+","
+                    +"'"+data['content'][x]['type']+"',"
+                    +"'"+"ENABLED"+"',"
+                    +desc+","
+                    +"'"+channel+"',"
+                    +"'en',"
+                    +"'"+data['content'][x]['content']+"',"
+                    +"null,"
+                    +"null,"
+                    +str(html)+","
+                    +str(0)+","
+                    +"now()"+","
+                    +"now(),"
+                    +"1,"
+                    +"null,"
+                    +"null,"
+                    +"null"
+                    +");\n\n")
 
 
 #Processing additional Languages
@@ -164,14 +174,14 @@ def cig():
 
     #throwing completion message to the GUI
     conclusionLabel = tk.Label(window, text='Done!')
-    conclusionLabel.config(font=('helvetica', 10))
+    conclusionLabel.config(font=('helvetica', 12))
     canvas.create_window(200, 270, window=conclusionLabel)
 
     #closing output file
     o.close()
 
 #declaring GUI button + action
-button = tk.Button(text='Generate Stripts!', command=cig, bg='brown', fg='white', font=('helvetica', 10, 'bold'))
+button = tk.Button(text='Generate Stripts!', command=cig, bg='black', fg='black', font=('helvetica', 12, 'bold'))
 canvas.create_window(200, 250, window=button)
 
 window.mainloop()
