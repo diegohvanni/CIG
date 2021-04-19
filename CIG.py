6072e902ca50b33f035ad7c48f1434d056de56fc
diegohvanni <diegoh.vanni@gmail.com>
2021-04-19 11:53:05 -0700

    v1.4
    I felt it was unnecessary to have 2 fields as identifiers and removed the content ticket number. The program number is also used on the output file.

--- CIG.py
+++ CIG.py
@@ -10,16 +10,16 @@ import tkinter as tk
 window = tk.Tk()
 window.title("CONTENT Inserts Generator (CIG)")
 processSource = tk.IntVar() #used for processing "en_source" file
-canvas = tk.Canvas(window, width = 450, height = 350,  relief = 'raised')
+canvas = tk.Canvas(window, width = 450, height = 300,  relief = 'raised')
 canvas.pack()
 
 title = tk.Label(window, text='CONTENT Inserts Generator (CIG)')
 title.config(font=('helvetica', 22))
 canvas.create_window(200, 25, window=title)
 
-fieldLabel1 = tk.Label(window, text='Enter the \"CONTENT\" ticket number:')
+fieldLabel1 = tk.Label(window, text='Enter the program number:')
 fieldLabel1.config(font=('helvetica', 12))
-canvas.create_window(100, 100, window=fieldLabel1)
+canvas.create_window(75, 100, window=fieldLabel1)
 
 entry1 = tk.Entry(window)
 canvas.create_window(100, 120, window=entry1)
@@ -31,23 +31,16 @@ canvas.create_window(90, 150, window=fieldLabel2)
 entry2 = tk.Entry(window)
 canvas.create_window(100, 170, window=entry2)
 
-fieldLabel3 = tk.Label(window, text='Enter the program number:')
-fieldLabel3.config(font=('helvetica', 12))
-canvas.create_window(75, 200, window=fieldLabel3)
-
-entry3 = tk.Entry(window)
-canvas.create_window(100, 220, window=entry3)
-
 fieldLabel4 = tk.Label(window, text='Process \"en_source\" file')
 fieldLabel4.config(font=('helvetica', 12))
-canvas.create_window(350, 150, window=fieldLabel4)
+canvas.create_window(350, 130, window=fieldLabel4)
 
 entry4 = tk.Checkbutton(window, variable=processSource, onvalue=1, offvalue=0)
-canvas.create_window(350, 170, window=entry4)
+canvas.create_window(350, 150, window=entry4)
 
 signature = tk.Label(window, text='by Diego H. Vanni - dvanni@paypal.com')
 signature.config(font=('helvetica', 12))
-canvas.create_window(350, 340, window=signature)
+canvas.create_window(350, 290, window=signature)
 
 # GUI ENDS
 
@@ -55,14 +48,13 @@ canvas.create_window(350, 340, window=signature)
 #defining "cig" function
 def cig():
 
-    ticketNumber = entry1.get() #getting Ticket Number from GUI
+    progNumber = entry1.get() #getting Ticket Number from GUI
     URL = entry2.get() #getting Folder Path from GUI
-    progNumber = entry3.get() #getting Program Number
 
 #MAIN PROCESSING STARTS
 
     #creating output file using "UTF-8" encoding to avoid issues with special char
-    o = open(URL+"/CONTENT - "+ticketNumber+" (Program "+progNumber+") "+" - DSQL.txt","w+", 1,"utf-8-sig", "Save File Error!") #generating UTF-8+BOM
+    o = open(URL+"/CONTENT for "+progNumber+" - DSQL.txt","w+", 1,"utf-8-sig", "Save File Error!") #generating UTF-8+BOM
 
     #capturing folder structure for all available languages
     languages = {}
@@ -189,13 +181,13 @@ def cig():
     #throwing completion message to the GUI
     conclusionLabel = tk.Label(window, text='Done!')
     conclusionLabel.config(font=('helvetica', 12))
-    canvas.create_window(225, 320, window=conclusionLabel)
+    canvas.create_window(225, 270, window=conclusionLabel)
 
     #closing output file
     o.close()
 
 #declaring GUI button + action
 button = tk.Button(text='Generate Stripts!', command=cig, bg='black', fg='black', font=('helvetica', 12, 'bold'))
-canvas.create_window(225, 300, window=button)
+canvas.create_window(225, 250, window=button)
 
 window.mainloop()
